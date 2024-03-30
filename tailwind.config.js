0bcedb5aced84bdd8cdbffd1bd252da325fad881
haffuz01 <haffuz01@gmail.com>
2024-03-30 11:08:42 +0300

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,svelte,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        jura: ["Jura", "Sans UI"],
+      },
+    },
+  },
+  plugins: [],
+};
